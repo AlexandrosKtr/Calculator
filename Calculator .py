261cb123a4da1b07fafb1f70d76be4a9c285b2bb
AlexandrosKtr <163065819+AlexandrosKtr@users.noreply.github.com>
2024-03-14 12:01:22 +0200

    Update Calculator .py

--- Calculator .py
+++ Calculator .py
@@ -26,6 +26,19 @@ def press_num_btn(num):
             num_display.config(text=calc_num2)
     erase_button.config(text='C')
 
+def press_op_btn(op):
+    global calc_num1
+    global calc_num2
+    global math_op
+    global equals_prior
+    if math_op != '' and calc_num2 != '' and not equals_prior:
+        calc_num1 = str(eval(calc_num1 + math_op + calc_num2))
+        num_display.config(text=calc_num1)
+    calc_num2 = ''
+    math_op = op
+    equals_prior = False
+    toggle_button()
+
 def toggle_button():
     if math_op == '/':
         divide_button.config(fg='#FF9912', bg='white', state=DISABLED)
@@ -200,22 +213,22 @@ erase_button = Button(root, text='AC', fg='black', bg='#F5F5DC', height=4, width
 erase_button.grid(row=1, column=0)
 Button(root, text='+/-', fg='black', bg='#F5F5DC', height=4, width=6, bd='3', command=press_neg).grid(row=1, column=1)
 Button(root, text='%', fg='black', bg='#F5F5DC', height=4, width=6, bd='3', command=press_percent).grid(row=1, column=2)
-divide_button = Button(root, text='/', fg='white', bg='#FF9912', height=4, width=6, bd='3', command=press_divide)
+divide_button = Button(root, text='/', fg='white', bg='#FF9912', height=4, width=6, bd='3', command=lambda: press_op_btn('/'))
 divide_button.grid(row=1, column=3)
 Button(root, text='7', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('7')).grid(row=2, column=0)
 Button(root, text='8', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('8')).grid(row=2, column=1)
 Button(root, text='9', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('9')).grid(row=2, column=2)
-multiply_button = Button(root, text='*', fg='white', bg='#FF9912', height=4, width=6, bd='3', command=press_multiply)
+multiply_button = Button(root, text='*', fg='white', bg='#FF9912', height=4, width=6, bd='3', command=lambda: press_op_btn('*'))
 multiply_button.grid(row=2, column=3)
 Button(root, text='4', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('4')).grid(row=3, column=0)
 Button(root, text='5', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('5')).grid(row=3, column=1)
 Button(root, text='6', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('6')).grid(row=3, column=2)
-minus_button = Button(root, text='-', fg='white', bg='#FF9912', height=4, width=6, bd='3', command=press_minus)
+minus_button = Button(root, text='-', fg='white', bg='#FF9912', height=4, width=6, bd='3', command=lambda: press_op_btn('-'))
 minus_button.grid(row=3, column=3)
 Button(root, text='1', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('1')).grid(row=4, column=0)
 Button(root, text='2', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('2')).grid(row=4, column=1)
 Button(root, text='3', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=lambda: press_num_btn('3')).grid(row=4, column=2)
-plus_button = Button(root, text='+', fg='white', bg='#FF9912', height=4, width=6, bd='3', command=press_plus)
+plus_button = Button(root, text='+', fg='white', bg='#FF9912', height=4, width=6, bd='3', command=lambda: press_op_btn('+'))
 plus_button.grid(row=4, column=3)
 Button(root, text='0', fg='white', bg='#4D4D4D', height=4, width=13, bd='3', command=lambda: press_num_btn('0')).grid(row=5, columnspan=2)
 Button(root, text=',', fg='white', bg='#4D4D4D', height=4, width=6, bd='3', command=press_comma).grid(row=5, column=2)
